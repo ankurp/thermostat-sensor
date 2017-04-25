932dc4a6b6f9c8dd7b445076296f578bb57d3df2
Ankur Patel <ankur.patel@ymail.com>
2017-04-25 09:15:19 -0400

    Converting to int

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 import os
 
 SERVER_DOMAIN = os.environ['SERVER_DOMAIN']
-BUTTON_PIN = os.environ['BUTTON_PIN']
-TEMP_SENSOR_PIN = os.environ['TEMP_SENSOR_PIN']
-REPORT_INTERVAL = os.environ['REPORT_INTERVAL']
+BUTTON_PIN = int(os.environ['BUTTON_PIN'])
+TEMP_SENSOR_PIN = int(os.environ['TEMP_SENSOR_PIN'])
+REPORT_INTERVAL = int(os.environ['REPORT_INTERVAL'])
