b03a4972dbd365feeebaaa1df61b81a48840089f
Ankur Patel <ankur.patel@ymail.com>
2017-04-25 12:54:18 -0400

    Updating start script

---
+++ Procfile
@@ -0,0 +1,2 @@
+alert: /usr/bin/python -u /usr/src/app/alert.py
+reading: /usr/bin/python -u /usr/src/app/reading.py
--- boot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-/home/pi/ngrok start ssh --config=/home/pi/.ngrok2/ngrok.yml > /dev/null &
-sleep 10 && /usr/bin/python /home/pi/thermostat-sensor/reading.py &
-/usr/bin/python /home/pi/thermostat-sensor/alert.py &
